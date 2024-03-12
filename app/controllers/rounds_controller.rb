0fac044d52fb06bae2aad14a86277785da1f892c
evandrotvc <evandro-tvc@hotmail.com>
2024-03-12 19:08:49 -0300

    round controller passando os testes, mas precisam melhorar

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,4 +1,6 @@
 class RoundsController < ApplicationController
+  before_action :set_round, only: %i[result show answers]
+
   def show
     @round = Round.find(params[:id])
 
@@ -8,7 +10,6 @@ class RoundsController < ApplicationController
   def create
     player = Player.find_by(name: round_params[:player_name])
     questions = Question.filter_category(round_params[:category_id]).sample(2)
-    byebug
 
     @round = Round.new(round_params.except(:player_name).merge(player: player, questions: questions))
 
@@ -20,23 +21,24 @@ class RoundsController < ApplicationController
   end
 
   def result
-    round = Round.find(params[:id])
-    render json: { round: { id: round.id, player_id: round.player_id,
-                            total_questions: round.total_questions, total_answered_questions: round.total_answered_questions, total_correct_answers: round.total_correct_answers } }
+    render :result
   end
 
   def answers
-    round = Round.find(params[:id])
-    @answer = round.answers.build(answer_params)
+    @answer = @round.answers.build(answer_params)
 
-    if answer.save
-      render :answers, status: :created
+    if @answer.save
+      render 'rounds/answers', status: :created
     else
-      render json: { errors: answer.errors }, status: :unprocessable_entity
+      render json: { errors: @answer.errors }, status: :unprocessable_entity
     end
   end
 
   private
+
+  def set_round
+    @round = Round.find(params[:id])
+  end
 
   def round_params
     params.require(:round).permit(:player_name, :category_id)
--- spec/requests/round_spec.rb
+++ spec/requests/round_spec.rb
@@ -15,20 +15,18 @@ RSpec.describe 'Rounds' do
       expect(json['round']).to include('id', 'player_id', 'questions', 'answers')
     end
   end
-# 
+
   describe 'POST /rounds' do
     it 'creates a new round' do
       player = create(:player)
       category = create(:portuguese_category)
-      question = create(:portuguese_question_2)
-      question2 = create(:portuguese_question_3)
-      byebug
+      # question = create(:portuguese_question_2)
+      # question2 = create(:portuguese_question_3)
       round_params = { round: { player_name: player.name, category_id: category.id } }
 
       post '/rounds', params: round_params, as: :json
       expect(response).to have_http_status(:created)
 
-      byebug
       # expect(json)
       expect(json).to have_key('round')
       expect(json['round']['player_id']).to eq(player.id)
@@ -36,33 +34,31 @@ RSpec.describe 'Rounds' do
     end
   end
 
-  # describe 'GET /rounds/:id/result' do
-  #   it 'returns the result of a round' do
-  #     round = create(:round_with_questions_and_answers)
-  #     get "/rounds/#{round.id}/result"
-  #     expect(response).to have_http_status(:success)
+  describe 'GET /rounds/:id/result' do
+    it 'returns the result of a round' do
+      round = create(:rounds_1)
+      get "/rounds/#{round.id}/result", as: :json
+      expect(response).to have_http_status(:success)
 
-  #     json_response = response.parsed_body
-  #     expect(json_response).to have_key('round')
-  #     expect(json_response['round']).to include('id', 'player_id', 'total_questions',
-  #       'total_answered_questions', 'total_correct_answers')
-  #   end
-  # end
+      expect(json).to have_key('round')
+      expect(json['round']).to include('id', 'player_id', 'total_questions',
+        'total_answered_questions', 'total_correct_answers')
+    end
+  end
 
-  # describe 'POST /rounds/:id/answers' do
-  #   it 'creates a new answer for a round' do
-  #     round = create(:round_with_questions)
-  #     question = round.questions.first
-  #     option = question.options.first
-  #     answer_params = { answer: { question_id: question.id, option_id: option.id } }
+  describe 'POST /rounds/:id/answers' do
+    it 'creates a new answer for a round' do
+      round = create(:rounds_1)
+      question = round.questions.first
+      option = question.options.first
+      answer_params = { answer: { question_id: question.id, option_id: option.id } }
 
-  #     post "/rounds/#{round.id}/answers", params: answer_params
-  #     expect(response).to have_http_status(:created)
+      post "/rounds/#{round.id}/answers", params: answer_params, as: :json
+      expect(response).to have_http_status(:created)
 
-  #     json_response = response.parsed_body
-  #     expect(json_response).to have_key('answer')
-  #     expect(json_response['answer']).to include('id', 'question_id', 'option_id',
-  #       'correct')
-  #   end
-  # end
+      expect(json).to have_key('answer')
+      expect(json['answer']).to include('id', 'question_id', 'option_id',
+        'correct')
+    end
+  end
 end
