1456ca7de77ab9d65674f3b1ade38333b5a1881b
evandrotvc <evandro-tvc@hotmail.com>
2024-03-12 14:50:13 -0300

    quase pronto factory questions

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -2,8 +2,16 @@ FactoryBot.define do
   factory :question do
   end
   factory :portuguese_question_1, parent: :question do
-    association :round
-    association :portuguese_category
+    association :round, factory: :rounds_1
+    association :category, factory: :portuguese_category
+
+    after(:build) do |question|
+        question.options << create(:portuguese_option1_q1, question: question)
+        question.options << create(:portuguese_option2_q1, question: question)
+        question.options << create(:portuguese_option3_q1, question: question)
+        question.options << create(:portuguese_option4_q1, question: question)
+    end
+
     description { 'o que é crase?' }
   end
 end
