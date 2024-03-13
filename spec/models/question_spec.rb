fa2d3e8f2bbe1da1a9b3b8477bc4b1f0457dacf1
evandrotvc <evandro-tvc@hotmail.com>
2024-03-13 15:26:38 -0300

    feat: adiciona test

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -9,4 +9,32 @@ RSpec.describe Question do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:description) }
   end
+
+  describe '#filter_category' do
+    let(:category) { create(:portuguese_category) }
+    let(:category_math) { create(:mathematic_category) }
+    let(:question) do
+      create(:portuguese_question_1, rounds: [], category:)
+    end
+    let(:question2) do
+      create(:portuguese_question_2, rounds: [], category:)
+    end
+
+    let(:question_math) do
+      create(:portuguese_question_2, rounds: [], category: category_math)
+    end
+
+    context 'when return only questions from portuguese' do
+      it 'has return questions' do
+        question
+        question2
+        question_math
+
+        questions_filtered = described_class.filter_category(category.id).ids
+
+        expect(questions_filtered).to include(question.id, question2.id)
+        expect(questions_filtered).not_to include(question_math.id)
+      end
+    end
+  end
 end
